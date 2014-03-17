8dc445b5983ae4eef231ac595ab7a09509d6ab73
Brian Castle <james.castle34@gmail.com>
2014-03-16 23:23:26 -0400

    corrected issue with selecting players by round

--- app/controllers/mm_teams_controller.rb
+++ app/controllers/mm_teams_controller.rb
@@ -139,6 +139,22 @@ class MmTeamsController < ApplicationController
     @players = MmTeam.get_rosters
   end
 
+  def get_eligible_players_by_round
+    @mm_team = MmTeam.find(get_team_for_current_user)
+    @selected_round = params[:selected_round]
+    seed_range = get_seed_ranges_by_round @selected_round.to_i
+    @ncaa_players = NcaaPlayer.get_players_by_seed_range(seed_range[0], seed_range[1])
+    render "shared/get_eligible_players_by_round"
+  end
+
+  def get_preferred_players_by_round
+    @mm_team = MmTeam.find(get_team_for_current_user)
+    @selected_round = params[:selected_round]
+    seed_range = get_seed_ranges_by_round @selected_round.to_i
+    @preferred_players = NcaaPlayer.get_preferred_players_by_seed_range_for_mm_team(get_team_for_current_user, seed_range[0], seed_range[1])
+    render "shared/get_preferred_players_by_round"
+  end
+
   private
 
   def verify_owner?
