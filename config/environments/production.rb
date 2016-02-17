52ae90ffb9e8e2848c27c8656fe383024b6c749b
Brian Castle <james.castle34@gmail.com>
2016-02-17 16:15:07 -0500

    oops....forgot to provide my domain for mail config in previous commit.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,7 @@ MarchMadness::Application.configure do
   :address        => ENV['MAILGUN_SMTP_SERVER'],
   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         => 'yourapp.heroku.com',
+  :domain         => 'quiet-citadel-86386.herokuapp.com',
   :authentication => :plain,
 }
 ActionMailer::Base.delivery_method = :smtp
