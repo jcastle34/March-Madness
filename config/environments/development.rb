03efd8aebc2009bdd8f8596f6426ca00dceacc48
Brian Castle <james.castle34@gmail.com>
2016-02-17 15:51:42 -0500

    Updated an issue with reset tokens for reseting passwords.  Added email server configuration for password resets.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ MarchMadness::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -26,17 +26,24 @@ MarchMadness::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
-  
+
   # Mailer configs
-  config.action_mailer.default_url_options = { :host => 'localhost:4000' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :enable_starttls_auto => true,
-    :address => 'smtp.gmail.com',
-    :port => '587',
-    :authentication => :plain,
-    :domain => 'ffblitz.com',
-    :user_name => 'james.castle34@gmail.com',
-    :password => 'My3Girls'
-  }
+  # config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  # config.action_mailer.delivery_method = :smtp
+
+  # config.action_mailer.smtp_settings = {
+  #   address: 'smtp.gmail.com',
+  #   port: 587,
+  #   domain: 'gmail.com',
+  #   authentication: 'plain',
+  #   enable_starttls_auto: true,
+  #   user_name: 'james.castle34@gmail.com',
+  #   password: ''
+  # }
+
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'no-reply@castle-sports.com'}
+  Rails.application.routes.default_url_options[:host] = 'localhost:3000'
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,10 +79,9 @@ MarchMadness::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   # Mailer configs
-  config.action_mailer.default_url_options = { :host => 'www.ffblitz.com' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :address => 'localhost',
-    :port => '25'
-  }
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'no-reply@castle-sports.com'}
+  Rails.application.routes.default_url_options[:host] = 'localhost:3000'
 end
