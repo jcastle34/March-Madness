c2ccb76302ec49c2a3596f5a8189f97a57151314
Brian Castle <james.castle34@gmail.com>
2016-02-17 16:23:20 -0500

    Updated production config for mail setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,12 +80,13 @@ MarchMadness::Application.configure do
 
   # Mailer configs
   ActionMailer::Base.smtp_settings = {
-  :port           => ENV['MAILGUN_SMTP_PORT'],
-  :address        => ENV['MAILGUN_SMTP_SERVER'],
-  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         => 'quiet-citadel-86386.herokuapp.com',
-  :authentication => :plain,
-}
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'quiet-citadel-86386.herokuapp.com',
+    :authentication => :plain,
+  }
 ActionMailer::Base.delivery_method = :smtp
+Rails.application.routes.default_url_options[:host] = 'quiet-citadel-86386.herokuapp.com'
 end
