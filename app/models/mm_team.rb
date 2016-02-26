84b9bb09fbe9c0f1af2cbb568e30a8b0efd6aaf8
Brian Castle <james.castle34@gmail.com>
2016-02-26 15:55:24 -0500

    Look and feel changes

--- app/models/mm_team.rb
+++ app/models/mm_team.rb
@@ -4,9 +4,8 @@ class MmTeam < ActiveRecord::Base
   has_many :ncaa_players
 
   attr_accessible :name
-  
+
   def get_players
-      # @ncaa_players = NcaaPlayer.joins('left outer join draft_picks dp on dp.ncaa_player_id = ncaa_players.id').where('dp.ncaa_player_id is NULL')
       @ncaa_players = NcaaPlayer.joins('left outer join draft_picks dp on dp.ncaa_player_id = ncaa_players.id
 left outer join player_scorings ps on ps.ncaa_player_id = ncaa_players.id').where('dp.mm_team_id = ?', self.id).group('ncaa_players.id')
   end
