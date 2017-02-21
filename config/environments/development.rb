3a2e5a71645c756490fb330f00ea120935ad14c8
Brian Castle <bcastle@brianc-mb.teamnorthwoods.com>
2017-02-21 16:37:50 -0500

    removed old mailer config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,19 +28,6 @@ MarchMadness::Application.configure do
   config.assets.debug = true
 
   # Mailer configs
-  # config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-  # config.action_mailer.delivery_method = :smtp
-
-  # config.action_mailer.smtp_settings = {
-  #   address: 'smtp.gmail.com',
-  #   port: 587,
-  #   domain: 'gmail.com',
-  #   authentication: 'plain',
-  #   enable_starttls_auto: true,
-  #   user_name: 'james.castle34@gmail.com',
-  #   password: ''
-  # }
-
   config.action_mailer.delivery_method = :sendmail
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
