8577e28f48a4369497808792b1c7021047c080bc
Brian Castle <bcastle@brianc-mb.teamnorthwoods.com>
2017-02-21 17:15:20 -0500

    Rails upgrade casualties.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,7 +2,7 @@ class AdminController < ApplicationController
   before_filter :user_is_admin?
 
   def index    
-      @ncaa_teams = NcaaTeam.find(:all, :order => "school")  
+      @ncaa_teams = NcaaTeam.all.order(:school)  
 			@ncaa_players = NcaaPlayer.joins('join ncaa_teams nt on nt.id = ncaa_players.ncaa_team_id').order('nt.school asc, ncaa_players.position desc')      
   end
 
