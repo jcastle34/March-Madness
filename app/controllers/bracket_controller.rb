161b18e110b4fc0aa9db933659074f4d28c05f6a
jcastle34 <james.castle34@gmail.com>
2017-02-23 15:23:11 -0500

    Updates for the bracket
    New method and check to make sure the bracket has been configured,
    before displaying it.  Added a new message to show when it has not been
    configured.

--- app/controllers/bracket_controller.rb
+++ app/controllers/bracket_controller.rb
@@ -1,12 +1,16 @@
 class BracketController < ApplicationController
 
   def index
-    @east_region_bracket_entries = matchup_seeds(BracketEntry.where(:region_id => 1).order(:seed))
-    @midwest_region_bracket_entries = matchup_seeds(BracketEntry.where(:region_id => 2).order(:seed))
-    @south_region_bracket_entries = matchup_seeds(BracketEntry.where(:region_id => 3).order(:seed))
-    @west_region_bracket_entries = matchup_seeds(BracketEntry.where(:region_id => 4).order(:seed))
-    @play_in_matchups = BracketEntry.where(:is_play_in => 1).order(:seed, :region_id)
-    p @play_in_matchups.inspect
+    if(BracketEntry.is_configured?)
+        @east_region_bracket_entries = matchup_seeds(BracketEntry.where(:region_id => 1).order(:seed))
+        @midwest_region_bracket_entries = matchup_seeds(BracketEntry.where(:region_id => 2).order(:seed))
+        @south_region_bracket_entries = matchup_seeds(BracketEntry.where(:region_id => 3).order(:seed))
+        @west_region_bracket_entries = matchup_seeds(BracketEntry.where(:region_id => 4).order(:seed))
+        @play_in_matchups = BracketEntry.where(:is_play_in => 1).order(:seed, :region_id)
+    else
+        redirect_to root_path
+        flash[:error] = t(:bracket_not_setup)
+    end
   end
 
   def update_regions
--- app/models/bracket_entry.rb
+++ app/models/bracket_entry.rb
@@ -3,4 +3,8 @@ class BracketEntry < ActiveRecord::Base
   belongs_to :region
   
   SEEDS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+
+  def self.is_configured?
+  	BracketEntry.all.count == 68
+  end
 end
