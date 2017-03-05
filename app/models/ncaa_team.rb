ed91e0b0116d55b46d3d59d8c5d975f5e286d8c5
jcastle34 <james.castle34@gmail.com>
2017-03-05 13:43:35 -0500

    Updates to the admin\ncaa teams
    - Added web id to add, edit, and list views, so it can be entered and
    modified as part of an ncaa team
    - Updated css to correct alignment issues

--- app/models/ncaa_team.rb
+++ app/models/ncaa_team.rb
@@ -2,7 +2,7 @@ class NcaaTeam < ActiveRecord::Base
   has_one :bracket_entry
   has_many :ncaa_players
 
-  attr_accessible :school, :nickname
+  attr_accessible :school, :nickname, :web_id
 
   def self.get_rosters
     NcaaTeam.joins(:ncaa_players).group(:ncaa_team_id).order(:school, "ncaa_players.position desc")
