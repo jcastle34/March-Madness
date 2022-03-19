865c2d96bb75430512c91b05839986f41a1be2fa
jcastle34 <james.castle34@gmail.com>
2022-03-19 01:35:28 -0400

    Added logic to deny new registrations if rosters are locked

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,12 @@
 class RegistrationsController < Devise::RegistrationsController
   
   def new
-    super
+    if(Draft.is_completed?)
+      flash[:alert] = t(:no_new_registrations)
+      redirect_to(new_user_session_path)
+    else
+      super
+    end
   end
 
   def edit
